f76c66bd749c2b9e1f478a2282dfa9282c41eb74
kavinmm-23EIR047 <kavinmm.23eie@kongu.edu>
2026-08-10 14:57:21 +0530

    fix: enforce build-time linting and type checking by removing ignore flags

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { typedRoutes: false },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   distDir: 'build',
 };
 module.exports = nextConfig;
